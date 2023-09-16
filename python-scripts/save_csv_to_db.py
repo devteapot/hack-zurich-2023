4e8375b6e0dff6332260deadbc1ca001b1e478e8
Michele Damian <39302292+damiam@users.noreply.github.com>
2023-09-16 04:27:31 +0200

    save to database (?)

---
+++ python-scripts/save_csv_to_db.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import firebase_admin
+from firebase_admin import firestore
+
+
+AVG_VEL = 50
+MAX_TIME = 10
+SLEEP = 8
+
+
+def get_firebase_db():
+    cred_obj = firebase_admin.credentials.Certificate('hack-zurich-2023-firebase-adminsdk-954o6-6d83532ec1.json')
+    default_app = firebase_admin.initialize_app(cred_obj)
+    
+    db = firestore.client()
+    return db
+
+
+def price_to_float(x):
+    if x == '-':
+        return 0
+    x = float(remove_aposto(str(x).split()[-1]))
+    return x
+
+
+def remove_aposto(x):
+    return float(str(x).replace('\'', ''))
+
+
+def get_data(country, dir):
+    filename = f'csv/{dir}bound {country}-Table 1.csv'
+    df = pd.read_csv(filename)
+    df['Freight Cost Per Ton [$/ton]'] = df['Freight Cost Per Ton [$/ton]'].apply(price_to_float)
+    df['Distance [km]'] = df['Distance [km]'].apply(remove_aposto)
+    return df
+
+
+def get_col_rename(old, new):
+    return {col: new[i] for i, col in enumerate(old)}
+
+
+def aggregate_plnts(df, dir):
+    final_cols = ['code', 'plant', 'lat', 'long']
+    trio_final_cols = ['code', 'lat', 'long']
+    if dir == 'In':
+        origin_cols = ['Origin Code', 'Origin', 'Origin Latitude', 'Origin Longitude']
+        dest_cols = ['Destination Code', 'Destination', 'Destination Latitude', 'Destination Longitude']
+        new_cols = get_col_rename(dest_cols, final_cols)
+        dest_df = df[dest_cols].drop_duplicates().rename(columns=new_cols)
+    else:
+        origin_cols = ['Plant', 'Plant Name', 'Plant Latitude', 'Plant Longitude']
+        dest_cols = ['Client Code', 'Client Latitude', 'Client Longitude']
+        new_cols = get_col_rename(dest_cols, trio_final_cols)
+        dest_df = df[dest_cols].drop_duplicates().rename(columns=new_cols)
+    new_cols = get_col_rename(origin_cols, final_cols)
+    origin_df = df[origin_cols].drop_duplicates().rename(columns=new_cols)
+    all_plants = pd.concat([origin_df, dest_df])
+    return all_plants
+
+
+def cook(db, all_plants):
+    batch = db.batch()
+    plants_ref = db.collection('plants')
+    
+    for _, row in all_plants.iterrows():
+        new_plant = plants_ref.document()
+        batch.set(new_plant, row.to_dict())
+    
+    batch.commit()
+
+
+def main():
+    db = get_firebase_db()
+    
+    in_arg = get_data('ARG', 'In')
+    all_plants = aggregate_plnts(in_arg, 'In')
+    cook(db, all_plants)
+    
+    out_arg = get_data('ARG', 'Out')
+    all_plants = aggregate_plnts(out_arg, 'Out')
+    cook(db, all_plants)
+    
+    in_mex = get_data('MEX', 'In')
+    all_plants = aggregate_plnts(in_mex, 'In')
+    cook(db, all_plants)
+    
+    out_mex = get_data('MEX', 'Out')
+    all_plants = aggregate_plnts(out_mex, 'Out')
+    cook(db, all_plants)
+
+
+if __name__ == '__main__':
+    main()
