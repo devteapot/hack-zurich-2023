693bfaee84e74bc1a6851d476ad91c281212a8d5
Michele Damian <39302292+damiam@users.noreply.github.com>
2023-09-17 07:56:40 +0200

    python stuff

--- python-scripts/save_csv_to_db.py
+++ python-scripts/save_csv_to_db.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import firebase_admin
 from firebase_admin import firestore
+from sys import maxsize
+from itertools import permutations
 
-
+V = 4
 AVG_VEL = 50
 MAX_TIME = 10
 SLEEP = 8
@@ -88,6 +90,41 @@ def main():
     all_plants = aggregate_plnts(out_mex, 'Out')
     cook(db, all_plants)
 
+ 
+# implementation of traveling Salesman Problem
+def travellingSalesmanProblem(graph, s):
+ 
+    # store all vertex apart from source vertex
+    vertex = []
+    for i in range(V):
+        if i != s:
+            vertex.append(i)
+ 
+    # store minimum weight Hamiltonian Cycle
+    min_path = maxsize
+    next_permutation=permutations(vertex)
+    for i in next_permutation:
+ 
+        # store current Path weight(cost)
+        current_pathweight = 0
+ 
+        # compute current path weight
+        k = s
+        for j in i:
+            current_pathweight += graph[k][j]
+            k = j
+        current_pathweight += graph[k][s]
+ 
+        # update minimum
+        min_path = min(min_path, current_pathweight)
+         
+    return min_path
+
 
 if __name__ == '__main__':
     main()
+    # matrix representation of graph
+    graph = [[0, 10, 15, 20], [10, 0, 35, 25],
+            [15, 35, 0, 30], [20, 25, 30, 0]]
+    s = 0
+    print(travellingSalesmanProblem(graph, s))
